7ae9c426723dda8a3ce96a74af342edbcc08c3ab
Prudvi Raju <prudvi2k7@gmail.com>
2022-05-22 13:31:07 +0530

    Initial commit added

---
+++ gatsby-config.ts
@@ -0,0 +1,16 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `My Gatsby Site`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-plugin-google-analytics',
+    options: {
+      "trackingId": ""
+    }
+  }]
+};
+
+export default config;
